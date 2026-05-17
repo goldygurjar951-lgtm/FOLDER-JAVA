1016a96209d1dbfc59a2b3ab3dd8e8d0953763c3
goldygurjar951-lgtm <goldygurjar951@gmail.com>
2026-05-17 13:10:54 +0530

    codes

---
+++ FOLDER_FOR_JAVA/frequencyinarray.java
@@ -0,0 +1,14 @@
+public class frequencyinarray {
+    public static void main(String[] args){
+        int[] arr={1,2,2,3,3,3,4,4,4,4};
+        int[] freq = new int[5];
+        for(int i=0;i<arr.length;i++){
+            freq[arr[i]]++;
+        }
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]>0){
+                System.out.println("Element: " + i + ", Frequency: " + freq[i]);
+            }
+        }
+    }
+}
