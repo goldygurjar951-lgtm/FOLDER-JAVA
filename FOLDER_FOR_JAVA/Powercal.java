498f4df0ac904001af9d245503b8c76e8865f2f6
goldygurjar951-lgtm <goldygurjar951@gmail.com>
2026-05-17 13:25:23 +0530

    codes

---
+++ FOLDER_FOR_JAVA/Powercal.java
@@ -0,0 +1,11 @@
+public class Powercal {
+    public static void main(String[] args){
+        int a=2;
+        int b=2;
+        int power=1;
+        for(int i=1;i<=b;i++){
+            power=power*a;
+        }
+        System.out.println(power);
+    }
+}
